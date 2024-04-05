c0f54329a7b971ddee115270655f052485902f19
abby-pfingsten <abbypfin@yahoo.com>
2024-04-05 10:55:32 -0400

    have path with routes in app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <React.StrictMode>
+    // <Header />
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" element={        <div>hi</div>
+} />
+        {/* <Route path="/survey" element={<Survey />} /> */}
+      </Routes>
+    </BrowserRouter>
+    // document.getElementById('root'),
+  )
 }
 
 export default App;
