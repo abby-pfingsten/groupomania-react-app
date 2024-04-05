6cd385ee6aba10ce6973c8cab6d8a80198283d06
abby-pfingsten <abbypfin@yahoo.com>
2024-04-05 10:06:22 -0400

    cannot get sequelize to work

---
+++ app/src/backend/app.js
@@ -0,0 +1,64 @@
+// // mongodb+srv://abbypfingsten:<password>@cluster0.47dfkhh.mongodb.net/?retryWrites=true&w=majority
+// // q1QVxPKmpLhPD5fF
+// // npm install mongodb@3.6
+// //  http://localhost:4200
+const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const mongodbErrorHandler = require("mongoose-mongodb-errors");
+// const path = require("path");
+
+// const userRoutes = require("./routes/user");
+// const sauceRoutes = require("./routes/sauce");
+
+// // const userRoutes = require("./routes/user");
+const app = express();
+
+
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://abbypfingsten:q1QVxPKmpLhPD5fF@cluster0.47dfkhh.mongodb.net/?retryWrites=true&w=majority"
+//   )
+//   .then(() => {
+//     console.log("Successful mongoose connection.");
+//   })
+
+//   .catch((error) => {
+//     console.log("Connection was unsuccessful");
+//     console.error(error);
+//   });
+
+// mongoose.plugin(mongodbErrorHandler);
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// // app.use("/api/stuff", stuffRoutes);
+// app.use("/images", express.static(path.join(__dirname, "images")));
+// app.use("/api/auth", userRoutes);
+// app.use("/api/sauces", sauceRoutes);
+
+// // need app.use for login?
+// module.exports = app;
