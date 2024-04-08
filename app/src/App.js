c74be8de659b898844fcf5ace40d0c729fe6267e
abby-pfingsten <abbypfin@yahoo.com>
2024-04-08 16:05:32 -0400

    have search changing url search params

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
+// import React, { useState } from 'react'
 // import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './frontend/pages/Home'
-import Layout from './frontend/pages/Layout'
+import Header from './frontend/pages/Header'
 import Profile from './frontend/pages/Profile'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Header />}>
           <Route exact path="/" element={<Home />} />
           <Route exact path="contact" element={<Profile />} />
         </Route>
--- app/src/frontend/pages/Home.js
+++ app/src/frontend/pages/Home.js
@@ -1,9 +1,11 @@
 function Home() {
-    return (
-        <div>
-            hi
-        </div>
-    )
+    //fetch some data and put it into a state variable
+  return (
+    <div>
+          <h1>Home Page</h1>
+          
+    </div>
+  )
 }
 
 export default Home
