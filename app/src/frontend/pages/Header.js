7f692c23eec9be7077fbfb9a2db978faf385cfb2
abby-pfingsten <abbypfin@yahoo.com>
2024-04-09 17:10:59 -0400

    restructuring seems to work

--- app/src/frontend/pages/Header.js
+++ app/src/frontend/pages/Header.js
@@ -1,17 +1,17 @@
-import { Outlet, Link } from 'react-router-dom'
-import '../styles/sass/Header.scss'
-import textLogo from '../images/icon-left-font-monochrome-black.svg'
-import iconLogo from '../images/icon.svg'
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { faX } from '@fortawesome/free-solid-svg-icons'
+import { Outlet, Link } from "react-router-dom"
+import "../styles/sass/Header.scss"
+import textLogo from "../images/icon-left-font-monochrome-black.svg"
+import iconLogo from "../images/icon.svg"
+import { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faX } from "@fortawesome/free-solid-svg-icons"
 // import { icon } from '@fortawesome/fontawesome-svg-core'
 
 function Header() {
   /* Constants */
   const urlParams = new URLSearchParams(window.location.search)
-  const [searchText, setSearchText] = useState(urlParams.get('search') ?? '')
+  const [searchText, setSearchText] = useState(urlParams.get("search") ?? "")
   const [click, setClick] = useState(false)
   const [mobileHeader, setMobileHeader] = useState(false)
 
@@ -24,11 +24,11 @@ function Header() {
   // the url
   const onChangeSearchText = () => {
     if (searchText.length !== 0) {
-      urlParams.set('search', searchText)
+      urlParams.set("search", searchText)
 
       window.location.search = urlParams
     } else {
-      window.location.search = ''
+      window.location.search = ""
     }
   }
   // set click to opposite
@@ -44,36 +44,29 @@ function Header() {
     }
   }
 
-  window.addEventListener('resize', showMobileHeader)
+  window.addEventListener("resize", showMobileHeader)
 
   return (
     <>
-      <nav>
-        <Link to="/">
-          <img
-            src={mobileHeader ? iconLogo : textLogo}
-            alt="Groupomania Logo"
-          ></img>
-        </Link>
-        <ul
+      <nav className="nav">
+        <div
           className={
-            mobileHeader ? 'nav-layout nav-layout__mobile' : 'nav-layout'
+            mobileHeader ? "nav-layout nav-layout__mobile" : "nav-layout"
           }
         >
-          <li>
-            {/* <Link to="/">
-              <img
-                src={mobileHeader ? iconLogo : textLogo}
-                alt="Groupomania Logo"
-              ></img>
-            </Link> */}
-          </li>
-          <li
+          <Link to="/" className="nav-layout__logo">
+            <img
+              src={mobileHeader ? iconLogo : textLogo}
+              alt="Groupomania Logo"
+            ></img>
+          </Link>
+
+          <div
             className={
-              mobileHeader ? 'nav-layout__search-mobile' : 'nav-layout__search'
+              mobileHeader ? "nav-layout__search-mobile" : "nav-layout__search"
             }
           >
-            {' '}
+            {" "}
             <input
               placeholder="Search Groupomania..."
               type="text"
@@ -81,13 +74,24 @@ function Header() {
               value={searchText}
             />
             <button type="submit" onClick={onChangeSearchText}>
-              Search{' '}
+              Search{" "}
             </button>
-          </li>
-          <li className="nav-layout__profile" onClick={handleClick}>
+          </div>
+          <div className="nav-layout__menu" onClick={handleClick}>
             <Link to="/profile">{click ? faXElement : faBarsElement} </Link>
-          </li>
-        </ul>
+          </div>
+          {/* 
+          <ul
+            className={
+              mobileHeader ? "nav-layout nav-layout__mobile" : "nav-layout"
+            }
+          > */}
+
+          {/* <li className="nav-layout__profile" onClick={handleClick}>
+              <Link to="/profile">{click ? faXElement : faBarsElement} </Link>
+            </li> */}
+          {/* </ul> */}
+        </div>
       </nav>
 
       <Outlet />
