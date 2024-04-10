453bf03b13c6e13d6663f3a99ea74986f3f00637
abby-pfingsten <abbypfin@yahoo.com>
2024-04-10 18:53:30 -0400

    input forms

--- app/src/frontend/pages/Signup.js
+++ app/src/frontend/pages/Signup.js
@@ -1,11 +1,28 @@
 import textLogo from "../images/icon-left-font-monochrome-black.svg"
+import "../styles/sass/_signup.scss"
 
 function Signup() {
   return (
     <>
-      <header>
-        <img src={textLogo}></img>
+      <header className="signup-header">
+        <img
+          src={textLogo}
+          alt="Groupomania Logo"
+          className="signup-header__logo"
+        ></img>
       </header>
+      <div className="signup-form">
+        <form className="signup-form">
+          <label>
+            Email:
+            <input type="text" label="Username"></input>
+          </label>
+          <label>
+            Password:
+            <input type="text"></input>
+          </label>
+        </form>
+      </div>
     </>
   )
 }
