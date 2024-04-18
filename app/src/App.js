496a974501e98bd680486c5403a3232ea4fe8d4b
abby-pfingsten <abbypfin@yahoo.com>
2024-04-18 16:33:29 -0400

    deleted dead code from app.js

--- app/src/App.js
+++ app/src/App.js
@@ -8,31 +8,22 @@ import Signup from "./frontend/pages/Signup"
 import Home from "./frontend/pages/Home"
 import Profile from "./frontend/pages/Profile"
 import { Navigate } from "react-router-dom"
-import { faTruckFieldUn } from "@fortawesome/free-solid-svg-icons"
 
 function App() {
-  // export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route exact path="account" element={<AccountHeader />} /> */}
         <Route exact path="/auth/login" element={<Login />} />
         <Route exact path="/auth/signup" element={<Signup />} />
-
-        {/* <Route path="/" element={<Header />}> */}
         <Route
           exact
           path="/"
           element={true ? <Home /> : <Navigate to="/auth/login" />}
         />
         <Route exact path="/profile" element={<Profile />} />
-        {/* </Route> */}
       </Routes>
     </BrowserRouter>
-    // document.getElementById('root'),
   )
 }
 
 export default App
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(<App />)
