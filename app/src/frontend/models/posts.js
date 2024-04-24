427806680e483d972e1eeee3846a79e098c300d8
abby-pfingsten <abbypfin@yahoo.com>
2024-04-24 17:15:50 -0400

    finally learned how to get mixins in

--- app/src/frontend/models/posts.js
+++ app/src/frontend/models/posts.js
@@ -1,5 +1,38 @@
  const userPosts = [
-  { title: "Title 1", body: "body 1 asdf asdf asdf asdf a", media: "media 1" },
-  { title: "Title 2", body: "body 1asdf asdfasdfasdfdfs", media: "media 1" },
-]
+   {
+     title: "Title 1",
+     body: "body 1 asdf asdfeeeeeeeeeeeeeewawrqwecrqwerqwerqwer qwr qer  asdf asdf a",
+     media: "media 1",
+   },
+   {
+     title: "Title 2",
+     body: "body 1asdf asdfeeeeeeeeeeeeeewawrqwecrqwerqwerqwer",
+     media: "media 1",
+   },
+   {
+     title: "Title 2",
+     body: "body 1asdf asdfeeeeeeeeeeeeeewawrqwecrqwerqwerqwer",
+     media: "media 1",
+   },
+   {
+     title: "Title 2",
+     body: "body 1asdf asdfeeeeeeeeeeeeeewawrqwecrqwerqwerqwer",
+     media: "media 1",
+   },
+   {
+     title: "Title 2",
+     body: "body 1asdf asdfeeeeeeeeeeeeeewawrqwecrqwerqwerqwer",
+     media: "media 1",
+   },
+   {
+     title: "Title 2",
+     body: "body 1asdf asdfeeeeeeeeeeeeeewawrqwecrqwerqwerqwer",
+     media: "media 1",
+   },
+   {
+     title: "Title 2",
+     body: "body 1asdf asdfeeeeeeeeeeeeeewawrqwecrqwerqwerqwer",
+     media: "media 1",
+   },
+ ]
 export default userPosts
--- app/src/frontend/pages/Home.js
+++ app/src/frontend/pages/Home.js
@@ -2,8 +2,22 @@ import "../styles/sass/_home.scss"
 import Header from "./Header"
 import userPosts from "../models/posts"
 import "../styles/sass/_posts.scss"
+import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+
 
 function Home() {
+
+
+   const [matches, setMatches] = useState(
+     window.matchMedia("(min-width: 768px)").matches
+   )
+
+   useEffect(() => {
+     window
+       .matchMedia("(min-width: 768px)")
+       .addEventListener("change", (e) => setMatches(e.matches))
+   }, [])
   //fetch some data and put it into a state variable
   return (
     <>
@@ -14,19 +28,19 @@ function Home() {
       </div>
       <div className="posts-div">
         {userPosts.map((post, index) => (
-          <section className="posts">
-            <div key={`${post.title}-${index}`} className="posts__title">
-              {post.title}
-            </div>
-            <div key={`${post.body}-${index}`} className="posts__body">
-              {post.body}
-            </div>
-            <div key={`${post.media}-${index}`} className="posts__media">
-              {post.media}
-            </div>
-
-            <br></br>
-          </section>
+          <Link className="posts-link">
+            <section className="posts">
+              <div key={`${post.title}-${index}`} className="posts__title">
+                {post.title}
+              </div>
+              <div key={`${post.body}-${index}`} className="posts__body">
+                {post.body}
+              </div>
+              <div key={`${post.media}-${index}`} className="posts__media">
+                {post.media}
+              </div>
+            </section>
+          </Link>
         ))}
       </div>
       {/* </ul> */}
