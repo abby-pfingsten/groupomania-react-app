81e4897e8a279bb05d12fe945c919240e45852f3
abby-pfingsten <abbypfin@yahoo.com>
2024-05-06 15:22:08 -0400

    commenting out to get post on frontend working

--- app/src/frontend/pages/Home.js
+++ app/src/frontend/pages/Home.js
@@ -1,11 +1,12 @@
 import "../styles/Home.scss"
 import Header from "./Header"
 import userPosts from "../models/posts"
+import axios from "axios"
 import "../styles/Posts.scss"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button } from "@mui/base"
 import Modal from "../components/Modal"
 
@@ -22,14 +23,31 @@ function Home({ isMobile }) {
   let userObject = localStorage.getItem("userInfo")
   let userName = JSON.parse(userObject)[["name"]]
 
+  // function getAllPosts() {
+  //   axios.get("http://localhost:3000/api/auth/").then((response) => {
+  //     console.log("Successfully grabbed all posts")
+  //   })
+  // }
+
+  // const [userPosts, setUserPosts] = useState({})
+  // useEffect(() => {
+  //   function getAllPosts() {
+  //     axios.get("http://localhost:3000/api/auth/").then((response) => {
+  //       console.log("Successfully grabbed all posts")
+  //       setUserPosts(response)
+  //     })
+  //   }
+  //   getAllPosts()
+  //   // setUserPosts(getAllPosts())
+  //   // const userPosts = getAllPosts()
+  //   //Runs on every render
+  // }, [])
+
   return (
     <>
       <Header isMobile={isMobile} />
       <div className="home">
         <h1>Welcome, {userName} </h1>
-        {/* <button className="home__button" onClick={() => setIsModalOpen(true)}>
-          {isMobile ? faCirclePlusElement : "Create A Post"}
-        </button> */}
         <Button
           variant="outlined"
           className="home__button"
