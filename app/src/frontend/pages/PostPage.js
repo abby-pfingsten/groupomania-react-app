ec2c8f774e16d89ed31d712a1388d524dfe47a40
abby-pfingsten <abbypfin@yahoo.com>
2024-05-12 14:22:37 -0400

    changing links you get the information from different posts

--- app/src/frontend/pages/PostPage.js
+++ app/src/frontend/pages/PostPage.js
@@ -10,6 +10,29 @@ import { Button } from "@mui/base"
 import Modal from "../components/Modal"
 
 function PostPage({ isMobile }) {
+  const userInfo = localStorage.getItem("userInfo")
+  const token = JSON.parse(userInfo)[["token"]]
+
+  const postId = window.location.pathname.slice(1)
+
+  useEffect(() => {
+    function getOnePost() {
+      axios
+        .get("http://localhost:3000/api/auth/" + postId, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          console.log("Successfully grabbed one post")
+          console.log(response.data)
+          //   setUserPosts(response.data)
+        })
+        .catch((error) => console.log(error))
+    }
+    getOnePost()
+  }, [token, postId])
+
   return (
     <>
       <Header isMobile={isMobile} />
