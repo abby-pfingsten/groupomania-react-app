080d63bb3e97566f4859ecc7d3a30879527642b7
abby-pfingsten <abbypfin@yahoo.com>
2024-05-21 16:32:01 -0400

    writing method to get one users information

--- app/src/frontend/pages/Account.js
+++ app/src/frontend/pages/Account.js
@@ -1,15 +1,41 @@
 import Header from "./Header"
 import "../styles/Account.scss"
+import axios from "axios"
+import { useState, useEffect } from "react"
+// import { useSearchParams } from "react-router-dom"
 
 const Account = ({ isMobile }) => {
   // grab the name of the user
   let userObject = localStorage.getItem("userInfo")
   let userName = JSON.parse(userObject)[["name"]]
+  let userId = JSON.parse(userObject)[["userId"]]
+  let token = JSON.parse(userObject)[["token"]]
 
   const handleDeleteAccount = () => {
     // Implement delete account logic here
     alert("Account deletion logic goes here.")
   }
+
+  const [userEmail, setUserEmail] = useState("")
+  const [userCreation, setUserCreation] = useState("")
+
+  useEffect(() => {
+    function getOneUser() {
+      axios
+        .get("http://localhost:3000/api/posts/" + userId, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          console.log("Successfully grabbed user information")
+          setUserEmail(response.data.email)
+          setUserEmail(response.data.createdAt)
+        })
+        .catch((error) => console.log(error))
+    }
+    getOneUser()
+  }, [token, userId])
 
   return (
     <>
