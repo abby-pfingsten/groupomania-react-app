5e6e53d8b4b17c1eb7c5de82bcaad0d3f74d320e
abby-pfingsten <abbypfin@yahoo.com>
2024-06-04 10:27:33 -0400

    included env-samples

--- app/src/backend/models/index.js
+++ app/src/backend/models/index.js
@@ -11,7 +11,7 @@ const db = {}
 const database = process.env.DB_NAME
 const username = process.env.DB_USERNAME
 const password = process.env.DB_PASSWORD
-console.log(process.env)
+
 let sequelize
 
 if (config.use_env_variable) {
---
+++ app/src/backend/schema.sql
@@ -0,0 +1,26 @@
+-- Creating the Users DB
+CREATE TABLE IF NOT EXISTS `Users` 
+(`id` INTEGER NOT NULL auto_increment , 
+`name` VARCHAR(255),
+ `email` VARCHAR(255) UNIQUE, 
+ `password` VARCHAR(255), 
+ `accountActive` VARCHAR(255), 
+ `createdAt` DATETIME NOT NULL, 
+ `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB;
+
+-- Creating the Posts DB
+CREATE TABLE IF NOT EXISTS `Posts` 
+(`id` INTEGER NOT NULL auto_increment ,
+ `title` VARCHAR(255), 
+ `body` VARCHAR(255), 
+ `media` VARCHAR(255), 
+ `usersRead` JSON, 
+ `createdAt` DATETIME NOT NULL, 
+ `updatedAt` DATETIME NOT NULL, 
+ `UserId` INTEGER NOT NULL, P
+ RIMARY KEY (`id`), 
+ FOREIGN KEY (`UserId`) 
+ REFERENCES `Users` (`id`) 
+ ON DELETE NO ACTION ON UPDATE CASCADE
+ ) ENGINE=InnoDB;
